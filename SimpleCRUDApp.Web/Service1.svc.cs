f5896578f4fbafb1fde7c0e7ebf7a5c334365c9c
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 15:33:50 +0200

    moved to GitHub

---
+++ SimpleCRUDApp.Web/Service1.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+
+namespace SimpleCRUDApp.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1
+    {
+        [OperationContract]
+        public List<Persons> GetPeople()
+        {
+            testmeEntities ctx = new testmeEntities();
+            var people = from p in ctx.Persons
+                         select p;
+
+            return people.ToList();
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
